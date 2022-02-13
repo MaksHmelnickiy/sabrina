ba685ec2a74e4df4b17d777f6fb0cfd046921e6c
mkhmelnytskiy <mkhmelnytskiy@easytech.co>
2022-02-13 03:40:28 +0100

    fix err

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,11 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalReset from './theme/reset';
 import IndexRoute from './routes';
 import GlobalFonts from './theme/globalFonts';
-import { mailer } from './node';
 
 
 const App = (): React.ReactElement => {
-  const onSend = () => {
-    mailer.send('WelcomeEmail', { firstName: 'Mathieu' }, {
-      to: 'my@email.com'
-    })
-  }
   return (
     <BrowserRouter>
-      <button onClick={onSend}>Heeeee</button>
       <GlobalReset />
       <GlobalFonts />
       <IndexRoute />
